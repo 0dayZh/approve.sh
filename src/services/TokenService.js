8f6065677a86e787726cfdcdcd6cd7b47a88b810
wangchen <wangchen@sparkpool.com>
2020-05-07 18:01:09 +0800

    Add some tokens

--- src/services/TokenService.js
+++ src/services/TokenService.js
@@ -33,4 +33,28 @@ export const tokens = [
     symbol: "bat",
     decimals: 18
   },
+  {
+    address: "0x1985365e9f78359a9B6AD760e32412f4a445E862",
+    name: "REP",
+    symbol: "rep",
+    decimals: 18
+  },
+  {
+    address: "0xe41d2489571d322189246dafa5ebde1f4699f498",
+    name: "ZRX",
+    symbol: "zrx",
+    decimals: 18
+  },
+  {
+    address: "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+    name: "SAI",
+    symbol: "sai",
+    decimals: 18
+  },
+  {
+    address: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+    name: "WBTC",
+    symbol: "wbtc",
+    decimals: 8
+  },
 ];
